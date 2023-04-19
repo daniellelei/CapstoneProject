66b5996b96d31191abc5b63fbf0a152346c943d0
Danielle Lei <Danielle.lei2014@gmail.com>
2023-04-19 14:14:07 -0700

    updated signup form

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms import StringField, DecimalField
+from wtforms.validators import DataRequired, Email, ValidationError, NumberRange
 from app.models import User
 
 
@@ -25,3 +25,4 @@ class SignUpForm(FlaskForm):
         'username', validators=[DataRequired(), username_exists])
     email = StringField('email', validators=[DataRequired(), user_exists])
     password = StringField('password', validators=[DataRequired()])
+    funds = DecimalField('funds', places=2, validators=[DataRequired(),NumberRange(min=1)])
