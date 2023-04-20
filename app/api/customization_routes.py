94b14a217605bc879fe57c2249245304a6d40739
Danielle Lei <Danielle.lei2014@gmail.com>
2023-04-19 17:10:23 -0700

    customization form done

--- app/api/customization_routes.py
+++ app/api/customization_routes.py
@@ -1,6 +1,10 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from app.models import Customization, User, db
 from flask_login import current_user, login_required
+from sqlalchemy.orm import joinedload
+from ..forms import CustomizationForm
+
+
 customization_routes = Blueprint('customizations', __name__)
 
 
@@ -9,7 +13,7 @@ def customizations():
     """
     Query for all customizations and returns them in a list of customizations dictionaries
     """
-    customizations = customization.query.all()
+    customizations = Customization.query.all()
     return {'customizations': [customization.to_dict() for customization in customizations]}
 
 
@@ -35,7 +39,69 @@ def get_user_customization():
 
 @customization_routes.route('/', methods=['POST'])
 @login_required
-def create_customization():
+def create_customization(drink_id, cart_id):
     user = current_user.to_dict()
     form = CustomizationForm()
     form["csrf_token"].data = request.cookies["csrf_token"]
+
+    if form.validate_on_submit():
+        new_customization = Customization(
+            user_id = user['id'],
+            cart_id = cart_id,
+            drink_id = drink_id,
+            size = form.data["Size"],
+            milk = form.data['Milk'],
+            shotOptions = form.data['Shot options'],
+            expressoRoastOptions=form.data['Expresso Roast Options'],
+        )
+        db.session.add(new_customization)
+        db.session.commit()
+        return {'Customization':new_customization.to_dict(), 
+                'User':new_customization.user.to_dict(), 
+                'Drink':new_customization.drink.to_dict(),
+                'Cart':new_customization.cart.to_dict()
+                }
+
+    if form.errors:
+        return {"message": "form errors", "errors": f"{form.errors}"}
+    return {"message": 'Bad Data'}
+
+
+@customization_routes.route('/<int:id>', methods=['PATCH', 'PUT'])
+@login_required
+def create_customization(id, drink_id, cart_id):
+    user = current_user.to_dict()
+    customization = Customization.query.get(id)
+
+    if customization.user_id == user["id"]:
+        form = CustomizationForm()
+        form["csrf_token"].data = request.cookies["csrf_token"]
+
+        if form.validate_on_submit():
+            customization.size = form.data["Size"]
+            customization.milk = form.data['Milk']
+            customization.shotOptions=form.data['Shot options']
+            customization.expressoRoastOptions = form.data['Expresso Roast Options']
+            
+            db.session.commit()
+            updated_customization = Customization.query.get(id)
+            return {'Customization': updated_customization.to_dict(),
+                    'User': updated_customization.user.to_dict(),
+                    'Drink': updated_customization.drink.to_dict(),
+                    'Cart': updated_customization.cart.to_dict()
+                    }
+
+        if form.errors:
+            return {"message": "form errors", "statusCode": 400, "errors": f"{form.errors}"}
+
+    return {"message": 'User does not own this customization'}
+
+@customization_routes.route("/<int:id>", methods=["DELETE"])
+@login_required
+def delete_customization(id):
+    customization = Customization.query.get(id)
+    if customization:
+        db.session.delete(customization)
+        db.session.commit()
+        return {"message": 'Customization Deleted!'}
+    return {"message": 'Customization not found'}
