9d4b20b1e2fdce59e39d659386b6cd2da14dcfd7
Danielle Lei <Danielle.lei2014@gmail.com>
2023-04-21 11:15:51 -0700

    working on cart route

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.drink_routes import drink_routes
 from .api.customization_routes import customization_routes
+from .api.cart_routes import cart_routes
 from .seeds import seed_commands
 from .config import Config
 
@@ -32,6 +33,7 @@ app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(drink_routes, url_prefix='/api/drinks')
 app.register_blueprint(customization_routes, url_prefix='/api/customizations')
+app.register_blueprint(cart_routes, url_prefix='/api/carts')
 db.init_app(app)
 Migrate(app, db)
 
