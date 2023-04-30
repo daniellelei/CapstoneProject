4de7edf38405690f9bf8f10bbda9d8d14cadd47d
Danielle Lei <Danielle.lei2014@gmail.com>
2023-04-29 23:16:50 -0700

    added button fixed

--- react-app/src/components/SingleDrink/confirmModal.js
+++ react-app/src/components/SingleDrink/confirmModal.js
@@ -1,29 +1,34 @@
 import React, { useState, useEffect } from "react";
-
+import * as drinkActions from '../../store/drink';
+import * as cartActions from '../../store/cart';
 import { useDispatch, useSelector } from "react-redux";
 import { useModal } from "../../context/Modal";
 import { useHistory } from "react-router-dom";
 
-function ConfirmModal() {
+function ConfirmModal({page}) {
   const dispatch = useDispatch();
   const { closeModal } = useModal();
   const history = useHistory();
 
   
-  const ClickNo = (e) => {
+  const ClickContinue = (e) => {
     e.preventDefault();
     closeModal();
+    history.push(page);
   };
+  const ClickGoToCart = (e) => {
+    e.preventDefault();
+    closeModal();
+    history.push('/cart')
+  }
 
   return (
     <div className="container">
-      <h3 className="title_text"> successfully Added to Order!</h3>
-
-      <div className="submitDiv de">
-        <button onClick={ClickNo} className="cancel-button">
-          close
-        </button>
-      </div>
+      <h3 className="title_text"> Successfully Added to Order!</h3>
+      <button onClick={ClickContinue}
+      >Continue</button>
+      <button onClick={ClickGoToCart}
+      >Go to Cart</button>
     </div>
   );
 }
--- react-app/src/components/SingleDrink/index.js
+++ react-app/src/components/SingleDrink/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
+import { useModal } from '../../context/Modal';
 import { NavLink, Redirect, useLocation, useParams, useHistory } from "react-router-dom";
 import * as drinkActions from '../../store/drink';
 import * as cartActions from '../../store/cart';
@@ -14,7 +15,7 @@ const Drink = () => {
     const {drinkId} = useParams();
     const history = useHistory();
     const [loading, setLoading] = useState(true);
-
+    const { setModalContent, setOnModalClose } = useModal();
     const dispatch = useDispatch();
     const drink = useSelector((state)=>state.drinks.singleDrink);
     const user = useSelector((state)=> state.session.user);
@@ -28,9 +29,28 @@ const Drink = () => {
             }, 1500);
         }
         dispatch(drinkActions.getDrinkDetailThunk(drinkId));
+        if(user) dispatch(cartActions.getCurrentCartThunk())
 
         return () => dispatch(drinkActions.actionClearDrink());
     },[dispatch, drinkId, loading])
+
+    const isAdded = (currentCart, drink) => {
+        if(!currentCart?.drinks?.length) return false;
+        for (let d of currentCart.drinks) {
+            if(d.id === drink.id) return true;
+        }
+        return false;
+    }
+    const numOfAdded = (currentCart, drink) => {
+        if (isAdded(currentCart, drink)===false) return 'Add'
+        let num = 0;
+        for (let d of currentCart.drinks) {
+            if(d.id === drink.id) {
+                num = num + 1
+            }
+        }
+        return `${num} in cart`
+    }
 
     if(!drink?.id || loading) return (<div className='loadingPage'>
         <img className="loadingImg" src="https://cdn.dribbble.com/users/2520294/screenshots/7209485/media/cf226d98a06282e9cabf5c2f8f6d547f.gif"/>
@@ -47,20 +67,46 @@ const Drink = () => {
                 <h2>{drink.name}</h2>
                 <p>Size: Grande</p>
                 <p>${drink.price}</p>
-                {user ? <button
+                
+                {user && !isAdded(currentCart,drink) ? <button
                 onClick = {async (e) => {
                     e.preventDefault();
                     if(user){
                         await dispatch(cartActions.addToCartThunk(drink))
                     }
+                    setModalContent(<ConfirmModal />);
                     }}
-                
                 >Add</button>
-                : <OpenModalButton 
+                : null} 
+                {!user ? <OpenModalButton 
                     buttonText= "Add to Cart"
-                    modalComponent={<SignUpLoginModal />}
-                />
-                }
+                    modalComponent={<SignUpLoginModal page={`/drinks/${drink.id}`}/>}
+                /> : null}
+                <div className='plusMinus'>
+                    {user && isAdded(currentCart, drink)? 
+                    <i 
+                    className="fa-solid fa-square-minus"
+                    onClick = { async(e) => {
+                        e.preventDefault();
+                        if(user){
+                            await dispatch(cartActions.removeFromCartThunk(drink))
+                        }
+                    }}
+                    ></i>
+                    : null}
+                    {isAdded(currentCart, drink)? <span className='numOfdrink'>{numOfAdded(currentCart, drink)}</span> : null}
+                    { user && isAdded(currentCart, drink)? 
+                    <i 
+                    className="fa-solid fa-square-plus"
+                    onClick = { async(e) => {
+                        e.preventDefault();
+                        if(user){
+                            await dispatch(cartActions.addToCartThunk(drink))
+                        }
+                    }}
+                    ></i>
+                    : null}
+                </div>
                  
 
             </div>
