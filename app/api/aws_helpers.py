f1c6250ef14e481bae332488af5567bcf58567f7
Danielle Lei <Danielle.lei2014@gmail.com>
2023-05-02 21:27:00 -0700

    setting up aws

---
+++ app/api/aws_helpers.py
@@ -0,0 +1,14 @@
+import boto3
+import botocore
+import os 
+import uuid 
+
+BUCKET_NAME=os.environ.get("S3_BUCKET")
+S3_LOCATION = f"https://{BUCKET_NAME}.s3.amazonaws.com/"
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif", "mp4", "mov"}
+
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id=os.environ.get("S3_KEY"),
+    aws_secret_access_key=os.environ.get("S3_SECRET"),
+)
