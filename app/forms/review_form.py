8978b9b08b6ad30bbfd3311f6ee51ab11671dace
Danielle Lei <Danielle.lei2014@gmail.com>
2023-05-24 16:24:28 -0700

    updated review form

--- app/forms/review_form.py
+++ app/forms/review_form.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, Length
 
 class ReviewForm (FlaskForm):
     reviewBody = StringField('reviewBody', validators=[DataRequired(), Length(min=3, 
                                                                               message = 'Review content needs to be at least 3 characters long.')])
+    rating = IntegerField('rating', validators=[DataRequired()])
     submit = SubmitField('Save Review')
