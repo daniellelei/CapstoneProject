6172116b88436858023808646b4d3e8355e29737
Danielle Lei <Danielle.lei2014@gmail.com>
2023-05-24 17:01:29 -0700

    updated edit review route

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint,request
 from ..forms.review_form import ReviewForm
 from flask_login import current_user, login_required
 from ..models import db, User, Post, Review
-
+from datetime import date
 
 review_routes = Blueprint('reviews', __name__)
 
@@ -26,6 +26,17 @@ def edit_review(id):
     if form.validate_on_submit():
         review.reviewBody = form.data['reviewBody']
         review.rating = form.data['rating']
+        review.date = date.today()
+        db.session.commit()
+        updated_review = Review.query.get(id)
+        return {**updated_review.to_dict()
+                , 'user': updated_review.user.to_dict()
+                , 'post': updated_review.post.to_dict()
+                }
+    
+    if form.errors:
+        return {"message": "form errors", "errors": f"{form.errors}"}
+    return {"message": 'Bad Data'}
 
 
 
